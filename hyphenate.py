103e160a568fb5cea74b0159e5a925dfcdb3329e
somenode <j@somenode.com>
2013-10-09 11:42:23 -0500

    updated line 2

--- hyphenate.py
+++ hyphenate.py
@@ -1,5 +1,5 @@
 fin = open('/path/to/your/textfile')
-fout = open('path/where/you_want/your_output', 'w')
+fout = open('/path/where/you_want/your_output', 'w')
 for line in fin: #runs through each line
 	word = line.strip() 
     	newline = word[0:2] + '-' + word[2:] + '\n' #this example inserts a hyphen after position 1 
